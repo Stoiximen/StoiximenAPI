2f4cc06fe7e9b6122ffcb71af970fe18c0495b89
GN <giwrgoskai1988@gmail.com>
2025-06-20 22:12:50 +0300

    .

--- Stoiximen.Infrastructure/Services/StoiximenConfiguration.cs
+++ Stoiximen.Infrastructure/Services/StoiximenConfiguration.cs
@@ -8,6 +8,7 @@ namespace Stoiximen.Infrastructure.Services
         public string TelegramBotToken { get; }
         public string TelegramChatId { get; }
         public string JwtSecretKey { get; }
+        public string JwtSalt { get; }
         public string JwtIssuer { get; }
         public string JwtAudience { get; }
         public int JwtExpirationMinutes { get; }
@@ -17,6 +18,7 @@ namespace Stoiximen.Infrastructure.Services
             TelegramBotToken = configuration["Security:TelegramBot:Token"] ?? throw new ArgumentNullException("TelegramBot:Token not configured");
             TelegramChatId = configuration["Security:TelegramBot:ChatId"] ?? string.Empty;
             JwtSecretKey = configuration["Security:Cryptography:AuthToken:Key"] ?? throw new ArgumentNullException("JWT Key not configured");
+            JwtSalt = configuration["Security:Cryptography:AuthToken:Salt"] ?? throw new ArgumentNullException("JWT Salt not configured");
             JwtIssuer = configuration["Security:Cryptography:AuthToken:Issuer"] ?? throw new ArgumentNullException("JWT Issuer not configured");
             JwtAudience = configuration["Security:Cryptography:AuthToken:Audience"] ?? throw new ArgumentNullException("JWT Audience not configured");
             JwtExpirationMinutes = int.Parse(configuration["Security:Cryptography:AuthToken:ExpirationMinutes"] ?? "60");
